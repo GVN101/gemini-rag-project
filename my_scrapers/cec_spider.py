6dfe7eada5f5fa53e7ba0bd5fcae24a09a5f77e0
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2024-12-15 22:30:25 +0530

    output file issues solved

--- my_scrapers/cec_spider.py
+++ my_scrapers/cec_spider.py
@@ -4,43 +4,51 @@ import json
 
 
 # spider to get the details from the admission section
-# class AdmissionsSpider(scrapy.Spider):
-#     name = 'admissions'
-#     allowed_domains = ['ceconline.edu']
-#     start_urls = ['https://ceconline.edu/admission/btech_admissions/']
+class AdmissionsSpider(scrapy.Spider):
+    name = 'admissions'
+    allowed_domains = ['ceconline.edu']
+    start_urls = ['https://ceconline.edu/admission/btech_admissions/']
 
-#     def parse(self, response):
+    total_admission_data = {}
 
-#         department_seat_capacity = {"department_wise_seat_capacity":{}} # main dictionary where all the admission data is 
+    def parse(self, response):
 
-#         # get the pdf that contains the document to be submitted
-#         admission_document_list_link = response.css('div.elementor-widget-container p a::attr(href)').get()
-#         page_header = response.css('div.wpb_wrapper h2::text').get()
-#         page_admission_description = response.css('div.wpb_wrapper p::text').get()
-#         seat_capacity_table = response.css('tr td span::text').getall()
+        department_seat_capacity = {"department_wise_seat_capacity":{}} # main dictionary where all the admission data is 
+
+        # get the pdf that contains the document to be submitted
+        admission_document_list_link = response.css('div.elementor-widget-container p a::attr(href)').get()
+        page_header = response.css('div.wpb_wrapper h2::text').get()
+        page_admission_description = response.css('div.wpb_wrapper p::text').get()
+        seat_capacity_table = response.css('tr td span::text').getall()
 
         
-#         for row in range(0,len(seat_capacity_table),2):
-#             department_seat_capacity['department_wise_seat_capacity'][seat_capacity_table[row]] = seat_capacity_table[row+1]
+        for row in range(0,len(seat_capacity_table),2):
+            department_seat_capacity['department_wise_seat_capacity'][seat_capacity_table[row]] = seat_capacity_table[row+1]
         
-#         btech_eligibility_criteria = response.css('ul li span::text').getall()
+        btech_eligibility_criteria = response.css('ul li span::text').getall()
 
-#         # i just converted the list into a single string
-#         wrapped_text = ""
-#         for word in btech_eligibility_criteria:
-#             wrapped_text += word
+        # i just converted the list into a single string
+        wrapped_text = ""
+        for word in btech_eligibility_criteria:
+            wrapped_text += word
 
-#         admission_data = {"admission_related_data":{}}
-#         admission_data['admission_related_data']['page_header'] = page_header
-#         admission_data['admission_related_data']['admission_description'] = page_admission_description
-#         admission_data['admission_related_data']['department_wise_seat_capacity'] = str(department_seat_capacity).replace('\u00a0','')
-#         admission_data['admission_related_data']['btech_eligibility_criteria'] = wrapped_text.replace('\u00a0','')
-#         admission_data['admission_related_data']['documents_to_be_submitted_during_admission_link'] = admission_document_list_link.replace(' ','%20')
+        admission_data = {"admission_related_data":{}}
+        admission_data['admission_related_data']['page_header'] = page_header
+        admission_data['admission_related_data']['admission_description'] = page_admission_description
+        admission_data['admission_related_data']['department_wise_seat_capacity'] = str(department_seat_capacity).replace('\u00a0','')
+        admission_data['admission_related_data']['btech_eligibility_criteria'] = wrapped_text.replace('\u00a0','')
+        admission_data['admission_related_data']['documents_to_be_submitted_during_admission_link'] = admission_document_list_link.replace(' ','%20')
+        self.total_admission_data["All about admission data"] = admission_data
 
-#         print(admission_data)
-#         print(type(admission_data))
-#         with open('output.json','w') as f:
-#             json.dump(admission_data,f)
+    def closed(self, response):
+
+        with open('output.json','r') as f:
+            existing_data = json.load(f)
+        existing_data.append(self.total_admission_data)
+
+        with open('output.json','w') as f:
+            json.dump(existing_data,f,indent=4)
+        
 
 class DepartmentSpider(scrapy.Spider):
     name = 'admissions'
@@ -95,8 +103,11 @@ class DepartmentSpider(scrapy.Spider):
 
     def closed(self, reason):
         # This method is called when the spider is closed
-        with open('output.json', 'a') as f:
-            json.dump(self.total_department_data, f, indent=4)  # Write the collected data to a JSON file
+        with open('output.json', 'r') as f:
+            existing_data  = json.load(f)
+            existing_data.append(self.total_department_data)
+        with open('output.json','w') as f:
+            json.dump(existing_data, f, indent=4)  # Write the collected data to a JSON file
 
 if __name__ == "__main__":
     from scrapy.crawler import CrawlerProcess
@@ -104,4 +115,5 @@ if __name__ == "__main__":
 
     process = CrawlerProcess(get_project_settings())
     process.crawl(DepartmentSpider) 
+    process.crawl(AdmissionsSpider)
     process.start()
