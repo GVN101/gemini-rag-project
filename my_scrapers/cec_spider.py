76ce16ab970ff381b97fe1663f30dd57be5bfe90
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2024-12-15 22:14:55 +0530

    webscraping fro department and others

--- my_scrapers/cec_spider.py
+++ my_scrapers/cec_spider.py
@@ -1,114 +1,107 @@
-import re
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+import scrapy
+import json
+# so basically we can add all our spider classes over here
 
-class CeconlineSpider(CrawlSpider):
-    name = 'ceconline'
+
+# spider to get the details from the admission section
+# class AdmissionsSpider(scrapy.Spider):
+#     name = 'admissions'
+#     allowed_domains = ['ceconline.edu']
+#     start_urls = ['https://ceconline.edu/admission/btech_admissions/']
+
+#     def parse(self, response):
+
+#         department_seat_capacity = {"department_wise_seat_capacity":{}} # main dictionary where all the admission data is 
+
+#         # get the pdf that contains the document to be submitted
+#         admission_document_list_link = response.css('div.elementor-widget-container p a::attr(href)').get()
+#         page_header = response.css('div.wpb_wrapper h2::text').get()
+#         page_admission_description = response.css('div.wpb_wrapper p::text').get()
+#         seat_capacity_table = response.css('tr td span::text').getall()
+
+        
+#         for row in range(0,len(seat_capacity_table),2):
+#             department_seat_capacity['department_wise_seat_capacity'][seat_capacity_table[row]] = seat_capacity_table[row+1]
+        
+#         btech_eligibility_criteria = response.css('ul li span::text').getall()
+
+#         # i just converted the list into a single string
+#         wrapped_text = ""
+#         for word in btech_eligibility_criteria:
+#             wrapped_text += word
+
+#         admission_data = {"admission_related_data":{}}
+#         admission_data['admission_related_data']['page_header'] = page_header
+#         admission_data['admission_related_data']['admission_description'] = page_admission_description
+#         admission_data['admission_related_data']['department_wise_seat_capacity'] = str(department_seat_capacity).replace('\u00a0','')
+#         admission_data['admission_related_data']['btech_eligibility_criteria'] = wrapped_text.replace('\u00a0','')
+#         admission_data['admission_related_data']['documents_to_be_submitted_during_admission_link'] = admission_document_list_link.replace(' ','%20')
+
+#         print(admission_data)
+#         print(type(admission_data))
+#         with open('output.json','w') as f:
+#             json.dump(admission_data,f)
+
+class DepartmentSpider(scrapy.Spider):
+    name = 'admissions'
     allowed_domains = ['ceconline.edu']
-    start_urls = ['https://ceconline.edu/']
+    start_urls = ['https://ceconline.edu/departments'] 
 
-    rules = (
-        Rule(LinkExtractor(allow=r'/.*'), callback='parse_page', follow=True),
-        Rule(LinkExtractor(allow=r'/administrators*'), callback='parse_administrations', follow=True)
-    )
+    total_department_data = {"about_departments": []}  # Change to a list to hold multiple department data
 
-    def parse_administrations(self, response):
-        designation = response.xpath(
-            'normalize-space(//div[contains(@class, "stm-teacher__position")]/text()]/text())'
-        ).get()
-        cleaned_designation = self.clean_text(designation)
+    def parse(self, response):
+        department_links = response.css('figure.wpb_wrapper.vc_figure a::attr(href)').getall()
+        for department_link in department_links:
+            yield response.follow(department_link, self.parse_department)  
 
-        staff_name = response.xpath(
-            'normalize-space(//div[contains(@class, "stm-teacher__name")]/text()]/text())'
-        ).get()
-        cleaned_staff_name = self.clean_text(staff_name)
-        return cleaned_designation, cleaned_staff_name
+    def parse_department(self, response):
+        department_data = {}  # current department data
 
-    def parse_page(self, response):
+        department_title = response.css('h2.vc_custom_heading.vc_do_custom_heading::text').get()
+        department_descriptions = response.css('div.wpb_text_column.wpb_content_element p::text').getall()
+        wrapped_department_description = " ".join(department_descriptions) 
 
-        self.parse_administrations(response)
+        department_data["department_title"] = department_title
+        department_data["about_the_department"] = wrapped_department_description.strip()  # Clean up whitespace
 
-        # Extract the title of the page
-        title = response.xpath(
-            'normalize-space(//div[contains(@class, "title")]/text() | //h2[contains(@class, "custom_heading")]/text())'
-        ).get()
-        cleaned_title = self.clean_text(title)
+        hod_details = {}
+        hod_details["HOD image url"] = response.css('figure.wpb_wrapper.vc_figure div img::attr(src)').get()
+        hod_details["HOD Name"] = response.css('div.stm-teacher-bio__text p span strong::text').get()
+        hod_details["HOD Designation"] = response.css('div.stm-teacher-bio__text p strong span::text').get()
+        hod_details["HOD Email"] = response.css('li.stm-contact-details__item.stm-contact-details__item_type_email a::text').get()
 
-        # Extract paragraphs (<p> tags) from the page
-        paragraphs = response.xpath('//p/text()').getall()
-        cleaned_paragraphs = [self.clean_text(p) for p in paragraphs]
+        department_data["About the HOD"] = hod_details
 
-        # Extract contact details (here only address bruh)
-        address = response.xpath('//li[contains(@class, "contact-details__item_type_address")]/text()').get()
-        phone = response.xpath('//li[contains(@class, "contact-details__item_type_tel")]/a/text()').get()
-        email = response.xpath('//li[contains(@class, "contact-details__item_type_email")]/a/text()').get()
+        total_faculy_list = []
+        # for CS faculty
+        faculty_list = response.css('td[width="319"] a::text,td[width="208"]::text,td[width="97"]::text').getall()
+        print(faculty_list)
 
-        # stm-contact-details__itemstm-contact-details__item stm-contact-details__item_type_fax
-        contact_details = {}
-        if address:
-            contact_details['address'] = self.clean_text(address)
-        if phone:
-            contact_details['phone'] = self.clean_text(phone)
-        if email:
-            contact_details['email'] = self.clean_text(email)
 
-        # Extract faculty details if present along wuth the HOD
-        faculty,hod = self.parse_faculty_table(response)
+        if faculty_list:
+            i = 2
+            print(faculty_list[i])
+            while(int(faculty_list[i])<30):
+                faculty_individual_data = {}
+                faculty_individual_data[faculty_list[i]] = {"faculty Name":faculty_list[i+1].replace('\u00a0',''), "faculty Designation": faculty_list[i+2]}
+                print(faculty_individual_data)
+                total_faculy_list.append(faculty_individual_data)
+                i+=3      
+        department_data["Department Faculty Data"] = total_faculy_list
 
-        # Yield structured data as JSON
-        data = {}
-        if title:
-            data['title'] = cleaned_title
-        if faculty:
-            data['faculty'] = faculty
-        if hod:
-            data['hod'] = hod
-        if paragraphs:
-            data['about'] = cleaned_paragraphs
-        if contact_details:
-            data['contact_details'] = contact_details
-        yield data
+        print("\n\n\n\n\n\n\n\n\n\n\n")
 
-    def parse_faculty_table(self, response):
-        # Locate the faculty table based on the headers
-        faculty_data = []
-        faculty_table = response.xpath('//h3[span[contains(text(), "Faculty")]]/following-sibling::table')
+        self.total_department_data["about_departments"].append(department_data)  # Append department data to the list
 
-        # scrape the hod data
-        hod_section = response.xpath('//div[contains(@class, "stm-teacher-bio__content")]')
-        if hod_section:
-            hod_name = hod_section.xpath('.//strong[contains(text(), "Dr.") or contains(text(), "Sri.") or contains(text(), "Smt.")]/text()').get()
-            hod_title = hod_section.xpath('.//span[contains(@style, "font-size: 12pt")]/text()').get()
-            department_address = hod_section.xpath('.//p/text()').getall()
-            department_address = [self.clean_text(line) for line in department_address if line.strip()]
+    def closed(self, reason):
+        # This method is called when the spider is closed
+        with open('output.json', 'a') as f:
+            json.dump(self.total_department_data, f, indent=4)  # Write the collected data to a JSON file
 
-            hod = {
-                'name': hod_name,
-                'title': hod_title,
-                'address': ' '.join(department_address)
-            }
-        else:
-            hod = None
+if __name__ == "__main__":
+    from scrapy.crawler import CrawlerProcess
+    from scrapy.utils.project import get_project_settings
 
-        # Skip the header row and parse each faculty row
-        rows = faculty_table.xpath('.//tr[position()>1]')
-        for row in rows:
-            name = self.clean_text(row.xpath('td[2]//text()').get())
-            designation = self.clean(row.xpath('td[3]//text()').get())
-
-            faculty_data_entry = {}
-            if name:
-                faculty_data_entry['name'] = name
-            if designation:
-                faculty_data_entry['designation'] = designation
-            faculty_data.append(faculty_data_entry)
-
-        return faculty_data,hod
-
-    def clean_text(self, text):
-        """Clean text by removing unnecessary spaces and non-ASCII characters."""
-        cleaned_text = text.strip() if text else ''
-        cleaned_text = re.sub(r'\s+', ' ', cleaned_text)  # Remove excessive spaces
-        cleaned_text = re.sub(r'[^\x00-\x7F]+', ' ', cleaned_text)  # Remove non-ASCII characters
-        return cleaned_text
-
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(DepartmentSpider) 
+    process.start()
